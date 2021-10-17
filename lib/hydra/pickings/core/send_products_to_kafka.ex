be9fa3e65f15918095390e4c16e2a33c7079af62
Harry <matheusthebr@gmail.com>
2021-10-17 19:49:46 -0300

    Send products to kafka.

---
+++ lib/hydra/pickings/core/send_products_to_kafka.ex
@@ -0,0 +1,12 @@
+defmodule Hydra.Pickings.Core.SendProductsToKafka do
+  @topic "k-picking"
+
+  def execute(products) do
+    client_id = :hydra_producer
+    hosts = [kafka: 9092]
+    :ok = :brod.start_client(hosts, client_id)
+    :ok = :brod.start_producer(client_id, @topic, [])
+
+    :brod.produce(client_id, @topic, 0, _key = "", Jason.encode!(products))
+  end
+end
--- lib/hydra_graphql/resolvers/picking_resolver.ex
+++ lib/hydra_graphql/resolvers/picking_resolver.ex
@@ -1,11 +1,8 @@
 defmodule HydraGraphql.Resolvers.PickingResolver do
+  alias Hydra.Pickings
+
   def create_picking(_parent, params, _resolution) do
-    # %{
-    #   products: [
-    #     %{product: "123", stores: ["123", "123123"]},
-    #     %{product: "123", stores: ["123", "123123"]}
-    #   ]
-    # }
+    Task.async(fn -> Pickings.create_picking(params) end)
 
     {:ok, "We received the payload, and we are going to create all orders!"}
   end
